98bc8474ed107f26f6006a1c82a849f5446263a6
Laszlo Csunderlik <csunderlik.laszlo88@gmail.com>
2024-01-24 13:06:45 +0100

    insideairbnb's custom hook, operators and sensors package.v2

--- airflow-insideairbnb/setup.py
+++ airflow-insideairbnb/setup.py
@@ -6,7 +6,7 @@ requirements = ["apache-airflow", "requests", "geojson"]
 setuptools.setup(
     name="airflow_insideairbnb",
     version="0.0.1",
-    description="Airflow DAGs for Inside Airbnb with custom hookd, iperators and sensors.",
+    description="insideairbnb's custom hook, operators and sensors package.",
     author="LCS",
     author_email="lcs@gmail.com",
     install_requires=requirements,
